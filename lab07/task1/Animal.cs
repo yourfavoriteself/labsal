ed1236b21c5fc7463311023ce69f510b9fceb69b
yourfavoriteself <113239587+yourfavoriteself@users.noreply.github.com>
2023-12-25 17:08:17 +0300

    add lab7

---
+++ lab07/task1/Animal.cs
@@ -0,0 +1,117 @@
+﻿using System;
+namespace task1
+{
+	public enum eFavoriteFood
+	{
+		Meat,
+		Plants,
+		Everything
+	}
+
+    [Comment("Общее животное")]
+	public abstract class Animal
+	{
+        public string Country { get; set; }
+        public bool HideFromOtherAnimals { get; set; }
+		public string Name { get; set; }
+		public eClassificationAnimal WhatAnimal;
+
+        public enum eClassificationAnimal
+        {
+            Herbivores,
+            Camivores,
+            Omnivores
+        }
+
+        public Animal()
+		{
+			
+		}
+
+		public void Deconstruct()
+		{
+
+		}
+
+		public eClassificationAnimal GetClassificationAnimal()
+		{
+			return WhatAnimal;
+		}
+
+		public abstract eFavoriteFood GetFavoriteFood();
+
+		public abstract void SayHello();
+	}
+
+    [Comment("Корова")]
+    public class Cow : Animal
+	{
+		public Cow()
+		{
+
+		}
+
+        public override eFavoriteFood GetFavoriteFood()
+        {
+            throw new NotImplementedException();
+        }
+
+        public override void SayHello()
+        {
+			Console.WriteLine("Муууу");
+        }
+    }
+
+    [Comment("Лев")]
+    public class Lion : Animal
+    {
+        public Lion()
+        {
+
+        }
+
+        public override eFavoriteFood GetFavoriteFood()
+        {
+            throw new NotImplementedException();
+        }
+
+        public override void SayHello()
+        {
+            Console.WriteLine("Я лев");
+        }
+    }
+
+    [Comment("Свинка")]
+    public class Pig : Animal
+    {
+        public Pig()
+        {
+
+        }
+
+        public override eFavoriteFood GetFavoriteFood()
+        {
+            throw new NotImplementedException();
+        }
+
+        public override void SayHello()
+        {
+            Console.WriteLine("Я свин");
+        }
+    }
+
+    public class Comment : Attribute
+    {
+        public string Commentary { get; }
+        public Comment()
+        {
+
+        }
+
+        public Comment(string comm)
+        {
+            Commentary = comm;
+        }
+    }
+}
+
---
+++ lab07/task1/Program.cs
@@ -0,0 +1,47 @@
+﻿using System.Reflection;
+using System.Xml.Linq; 
+
+namespace task1
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            Lion lion = new();
+            lion.Country = "Ethiopia";
+            lion.HideFromOtherAnimals = false;
+            lion.WhatAnimal = Animal.eClassificationAnimal.Camivores;
+            lion.Name = "lion";
+            Pig pig = new();
+            pig.Country = "Everywhere";
+            pig.HideFromOtherAnimals = false;
+            pig.WhatAnimal = Animal.eClassificationAnimal.Herbivores;
+            pig.Name = "pig";
+            Cow cow = new();
+            cow.Country = "Everywhere";
+            cow.HideFromOtherAnimals = false;
+            cow.WhatAnimal = Animal.eClassificationAnimal.Herbivores;
+            cow.Name = "cow";
+
+            List<Animal> animals = new(3);
+            animals.Add(lion);
+            animals.Add(pig);
+            animals.Add(cow);
+
+            XDocument doc = new(new XElement("animals"));
+
+            foreach (var animal in animals)
+            {
+                XElement element = new(new XElement("animal",
+                    new XElement("type", animal.GetType().ToString()),
+                    new XElement("hideFromOtherAnimals", animal.HideFromOtherAnimals.ToString()),
+                    new XElement("country", animal.Country),
+                    new XElement("comment", ((Comment)animal.GetType().GetCustomAttribute(typeof(Comment))).Commentary)));
+                doc.Root.Add(element);
+            }
+
+            doc.Save("animals.xml");
+            Console.WriteLine(doc);
+        }
+    }
+}
